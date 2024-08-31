d737b7ce854c62d4418dbe9b334ed95ca3c7c5e1
Crowdbotics <team@crowdbotics.com>
2024-08-31 18:21:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "travely-49231",
+  url: "https://travely-49231.botics.co",
+  api: "https://travely-49231.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
